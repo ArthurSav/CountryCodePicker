c96de8168ff04ab288d438407f12f62370412368
Arthur Saveliev <arthur.saveliev@gmail.com>
2017-05-16 11:51:46 +0200

    - picker now can show a specific list of countries instead of all

--- CountryCodePicker/src/com/sithagi/countrycodepicker/CountryPicker.java
+++ CountryCodePicker/src/com/sithagi/countrycodepicker/CountryPicker.java
@@ -1,22 +1,12 @@
 package com.sithagi.countrycodepicker;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Currency; 
-import java.util.List;
-import java.util.Locale;
-
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Base64; 
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,178 +14,210 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.EditText;
 import android.widget.ListView;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Currency;
+import java.util.List;
+import java.util.Locale;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-public class CountryPicker extends DialogFragment implements
-		Comparator<Country> {
-	
-	private EditText searchEditText;
-	private ListView countryListView;
+public class CountryPicker extends DialogFragment implements Comparator<Country> {
 
-	private CountryListAdapter adapter;
+  public static final String EXTRA_COUNTRIES = "countries";
+  public static final String EXTRA_TITLE = "title";
+  private String[] displayedCountriesCodes;
 
-	private List<Country> allCountriesList;
+  private EditText searchEditText;
+  private ListView countryListView;
 
-	private List<Country> selectedCountriesList;
+  private CountryListAdapter adapter;
 
-	private CountryPickerListener listener;
+  private List<Country> allCountriesList;
+  private List<Country> selectedCountriesList;
 
-	public void setListener(CountryPickerListener listener) {
-		this.listener = listener;
-	}
+  private CountryPickerListener listener;
 
-	public EditText getSearchEditText() {
-		return searchEditText;
-	}
+  public void setListener(CountryPickerListener listener) {
+    this.listener = listener;
+  }
 
-	public ListView getCountryListView() {
-		return countryListView;
-	}
+  public EditText getSearchEditText() {
+    return searchEditText;
+  }
 
-	public static Currency getCurrencyCode(String countryCode) {
-		try {
-			return Currency.getInstance(new Locale("en", countryCode));
-		} catch (Exception e) {
+  public ListView getCountryListView() {
+    return countryListView;
+  }
 
-		}
-		return null;
-	}
+  public static Currency getCurrencyCode(String countryCode) {
+    try {
+      return Currency.getInstance(new Locale("en", countryCode));
+    } catch (Exception e) {
 
-	private List<Country> getAllCountries() {
-		if (allCountriesList == null) {
-			try {
-				allCountriesList = new ArrayList<Country>();
-				
-				String allCountriesCode = readEncodedJsonString(getActivity());
-				
-				JSONArray countrArray = new JSONArray(allCountriesCode);
-				
-				for (int i = 0; i < countrArray.length(); i++) {
-					JSONObject jsonObject = countrArray.getJSONObject(i);
-					String countryName = jsonObject.getString("name");
-					String countryDialCode = jsonObject.getString("dial_code");
-					String countryCode = jsonObject.getString("code");
-					
-					Country country = new Country();
-					country.setCode(countryCode);
-					country.setName(countryName);
-					country.setDialCode(countryDialCode);
-					allCountriesList.add(country);
-				}
+    }
+    return null;
+  }
 
-				Collections.sort(allCountriesList, this);
+  private List<Country> getCountriesFromFile() {
+    if (allCountriesList == null) {
+      try {
+        allCountriesList = new ArrayList<Country>();
 
-				selectedCountriesList = new ArrayList<Country>();
-				selectedCountriesList.addAll(allCountriesList);
+        String allCountriesCode = readEncodedJsonString(getActivity());
 
-				// Return
-				return allCountriesList;
+        JSONArray countrArray = new JSONArray(allCountriesCode);
 
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-		return null;
-	}
+        for (int i = 0; i < countrArray.length(); i++) {
+          JSONObject jsonObject = countrArray.getJSONObject(i);
+          String countryName = jsonObject.getString("name");
+          String countryDialCode = jsonObject.getString("dial_code");
+          String countryCode = jsonObject.getString("code");
 
-	private static String readEncodedJsonString(Context context)
-			throws java.io.IOException {
-		String base64 = context.getResources().getString(R.string.countries_code);
-		byte[] data = Base64.decode(base64, Base64.DEFAULT);
-		return new String(data, "UTF-8");
-	}
+          Country country = new Country();
+          country.setCode(countryCode);
+          country.setName(countryName);
+          country.setDialCode(countryDialCode);
+          allCountriesList.add(country);
+        }
 
-	/**
-	 * To support show as dialog
-	 * 
-	 * @param dialogTitle
-	 * @return
-	 */
-	public static CountryPicker newInstance(String dialogTitle) {
-		CountryPicker picker = new CountryPicker();
-		Bundle bundle = new Bundle();
-		bundle.putString("dialogTitle", dialogTitle);
-		picker.setArguments(bundle);
-		return picker;
-	}
- 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-			Bundle savedInstanceState) { 
-		View view = inflater.inflate(R.layout.country_picker, null);
- 
-		getAllCountries();
- 
-		Bundle args = getArguments();
-		if (args != null) {
-			String dialogTitle = args.getString("dialogTitle");
-			getDialog().setTitle(dialogTitle);
+        Collections.sort(allCountriesList, this);
 
-			int width = getResources().getDimensionPixelSize(
-					R.dimen.cp_dialog_width);
-			int height = getResources().getDimensionPixelSize(
-					R.dimen.cp_dialog_height);
-			getDialog().getWindow().setLayout(width, height);
-		}
- 
-		searchEditText = (EditText) view
-				.findViewById(R.id.country_code_picker_search);
-		countryListView = (ListView) view
-				.findViewById(R.id.country_code_picker_listview);
- 
-		adapter = new CountryListAdapter(getActivity(), selectedCountriesList);
-		countryListView.setAdapter(adapter);
- 
-		countryListView.setOnItemClickListener(new OnItemClickListener() {
+        selectedCountriesList = new ArrayList<Country>();
+        selectedCountriesList.addAll(allCountriesList);
 
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view,
-					int position, long id) {
-				if (listener != null) {
-					Country country = selectedCountriesList.get(position);
-					listener.onSelectCountry(country.getName(),
-							country.getCode(),country.getDialCode());
-				}
-			}
-		});
- 
-		searchEditText.addTextChangedListener(new TextWatcher() {
+        // Return
+        return allCountriesList;
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    return null;
+  }
 
-			@Override
-			public void onTextChanged(CharSequence s, int start, int before,
-					int count) {
-			}
+  private static String readEncodedJsonString(Context context) throws java.io.IOException {
+    String base64 = context.getResources().getString(R.string.countries_code);
+    byte[] data = Base64.decode(base64, Base64.DEFAULT);
+    return new String(data, "UTF-8");
+  }
 
-			@Override
-			public void beforeTextChanged(CharSequence s, int start, int count,
-					int after) {
-			}
+  /**
+   * To support show as dialog
+   */
+  public static CountryPicker newInstance(String title) {
+    CountryPicker picker = new CountryPicker();
+    Bundle bundle = new Bundle();
+    bundle.putString(EXTRA_TITLE, title);
+    picker.setArguments(bundle);
+    return picker;
+  }
 
-			@Override
-			public void afterTextChanged(Editable s) {
-				search(s.toString());
-			}
-		});
+  /**
+   * Use this instance to restrict displated countries by providing the country code to be displayed
+   * @param title dialog title
+   * @param countries countries to show e.g Countries[GB, GR, ES]
+   * @return dialog picker
+   */
+  public static CountryPicker newInstance(String title, String[] countries) {
+    Bundle args = new Bundle();
+    args.putStringArray(EXTRA_COUNTRIES, countries);
+    args.putString(EXTRA_TITLE, title);
+    CountryPicker fragment = new CountryPicker();
+    fragment.setArguments(args);
 
-		return view;
-	} 
-	
-	@SuppressLint("DefaultLocale")
-	private void search(String text) {
-		selectedCountriesList.clear();
+    return fragment;
+  }
 
-		for (Country country : allCountriesList) {
-			if (country.getName().toLowerCase(Locale.ENGLISH)
-					.contains(text.toLowerCase())) {
-				selectedCountriesList.add(country);
-			}
-		}
+  @Override public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
+    View view = inflater.inflate(R.layout.country_picker, null);
 
-		adapter.notifyDataSetChanged();
-	}
- 
-	@Override
-	public int compare(Country lhs, Country rhs) {
-		return lhs.getName().compareTo(rhs.getName());
-	}
+    getCountriesFromFile();
 
+    Bundle args = getArguments();
+    if (args != null) {
+      String dialogTitle = args.getString(EXTRA_TITLE);
+      getDialog().setTitle(dialogTitle);
+
+      displayedCountriesCodes = args.getStringArray(EXTRA_COUNTRIES);
+
+      int width = getResources().getDimensionPixelSize(R.dimen.cp_dialog_width);
+      int height = getResources().getDimensionPixelSize(R.dimen.cp_dialog_height);
+      getDialog().getWindow().setLayout(width, height);
+    }
+
+    if (displayedCountriesCodes != null) {
+      List<Country> filteredCountries = getCountriesWithCodes(displayedCountriesCodes);
+      this.allCountriesList = filteredCountries;
+      this.selectedCountriesList = filteredCountries;
+    }
+
+    searchEditText = (EditText) view.findViewById(R.id.country_code_picker_search);
+    countryListView = (ListView) view.findViewById(R.id.country_code_picker_listview);
+
+    adapter = new CountryListAdapter(getActivity(), selectedCountriesList);
+    countryListView.setAdapter(adapter);
+
+    countryListView.setOnItemClickListener(new OnItemClickListener() {
+
+      @Override public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if (listener != null) {
+          Country country = selectedCountriesList.get(position);
+          listener.onSelectCountry(country.getName(), country.getCode(), country.getDialCode());
+        }
+      }
+    });
+
+    searchEditText.addTextChangedListener(new TextWatcher() {
+
+      @Override public void onTextChanged(CharSequence s, int start, int before, int count) {
+      }
+
+      @Override public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+      }
+
+      @Override public void afterTextChanged(Editable s) {
+        search(s.toString());
+      }
+    });
+
+    return view;
+  }
+
+
+
+  private List<Country> getCountriesWithCodes(String[] countryCodes){
+    List<Country> filtered = new ArrayList<>();
+    for (String code : countryCodes) {
+      Country country = getCountryByCode(code);
+      if (country != null) filtered.add(country);
+    }
+    return filtered;
+  }
+
+  private Country getCountryByCode(String countryCode){
+    for (Country country : allCountriesList) {
+      if (country.getCode().equalsIgnoreCase(countryCode)) {
+        return country;
+      }
+    }
+    return null;
+  }
+
+  @SuppressLint("DefaultLocale") private void search(String text) {
+    selectedCountriesList.clear();
+
+    for (Country country : allCountriesList) {
+      if (country.getName().toLowerCase(Locale.ENGLISH).contains(text.toLowerCase())) {
+        selectedCountriesList.add(country);
+      }
+    }
+
+    adapter.notifyDataSetChanged();
+  }
+
+  @Override public int compare(Country lhs, Country rhs) {
+    return lhs.getName().compareTo(rhs.getName());
+  }
 }
